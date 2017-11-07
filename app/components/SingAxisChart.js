a71c77feb46448692b38827c26d4422204c79c9c
康宁 <kangweichen-rj@ofo.com>
2017-11-07 13:02:58 +0800

    半边圆角问题等待解决

--- app/components/SingAxisChart.js
+++ app/components/SingAxisChart.js
@@ -18,35 +18,36 @@ export default class SingleAxisChart extends Component {
     const data = this.sort(this.props.data.number);
     const titles = this.props.data.titles;
     return (
-      <View style={styles.container}>
-        <View>
-          <Text>{titles[0]}</Text>
-        <View style={styles.item_container}>
-          <View
-            onLayout={({nativeEvent: e}) => {
-              this.setState({maxLength: e.layout.width});
-            }}
-            ref='topOne' style={styles.topOne}/>
-          <Text>{data[ 0 ].num}</Text>
-        </View>
-        </View>
-        {
-          data.map((item, index) => {
-            let width = data[ index ].ratio * this.state.maxLength;
-            console.log(width);
-            console.log(this.state.maxLength);
-            return index > 0 ? <View>
-              <Text>{titles[index]}</Text>
-              <View style={styles.item_container}>
-                <View ref='top' style={{
-                  backgroundColor: '#FDE000',
-                  width: width,
-                  height: 12
-                }}/>
-              <Text>{data[ index ].num}</Text>
-              </View></View> : null;
-          })}
-      </View>
+      data && data.length > 0 ?
+        <View style={styles.container}>
+          <View>
+            <Text style={styles.textTitle}>{titles[0]}</Text>
+            <View style={styles.item_container}>
+              <View
+                onLayout={({nativeEvent: e}) => {
+                  this.setState({maxLength: e.layout.width});
+                }}
+                ref='topOne' style={styles.topOne}/>
+              <Text style={styles.textNumber}>{data[0].num}</Text>
+            </View>
+          </View>
+          {
+            data.map((item, index) => {
+              let width = data[index].ratio * this.state.maxLength;
+              console.log(width);
+              console.log(this.state.maxLength);
+              return index > 0 ? <View>
+                <Text style={styles.textTitle}>{titles[index]}</Text>
+                <View style={styles.item_container}>
+                  <View ref='top' style={{
+                    backgroundColor: '#FDE000',
+                    width: width,
+                    height: 12
+                  }}/>
+                  <Text style={styles.textNumber}>{data[index].num}</Text>
+                </View></View> : null;
+            })}
+        </View> : null
     );
   }
   
@@ -70,21 +71,31 @@ export default class SingleAxisChart extends Component {
 
 const styles = StyleSheet.create({
   container: {
+    paddingLeft: 16,
+    paddingRight: 16,
     flexDirection: 'column',
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
   },
   item_container: {
-    height: 37,
+    marginTop: 8,
     flexDirection: 'row',
   },
   topOne: {
     flex: 1,
     height: 12,
-    backgroundColor: '#FDE000'
+    backgroundColor: '#FDE000',
+    borderRadius: 10,
   },
-  text: {
+  textTitle: {
+    marginTop: 17,
     marginLeft: 8,
     marginRight: 16,
     fontSize: 12
+  },
+  textNumber: {
+    marginLeft: 8,
+    marginRight: 16,
+    fontSize: 12,
+    fontWeight:'bold'
   }
 });
